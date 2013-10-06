3589ea086855c0eced6ab8207ceb027933526af6
yuyunko <i.xxxxxxxxxxxxx.13@gmail.com>
2013-10-06 19:59:27 +0900

    End Lesson 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,16 +4,26 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
+
+    let(:heading) { 'Sample App' }
+    let(:page_title) { 'Help' }
 
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
@@ -22,6 +32,9 @@ describe "Static pages" do
   describe "About page" do
     before { visit about_path }
 
+    let(:heading) { 'Sample App' }
+    let(:page_title) { 'About Us' }
+
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
   end
@@ -29,7 +42,25 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
+    let(:heading) { 'Sample App' }
+    let(:page_title) { 'Contact' }
+
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
